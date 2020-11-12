210a88535637c222336eecbf5a0a214351235887
Aldijana Joffe <aldijanajoffe@gmail.com>
2020-11-12 15:33:06 +0100

    update js

--- index.js
+++ index.js
@@ -80,7 +80,8 @@ function search(city) {
   
   axios.get(apiUrl).then(displayTemperature);
 
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?
+  lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
