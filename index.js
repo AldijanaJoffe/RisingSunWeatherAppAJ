0b65fd7047dace9818ddbc150e6c85f1e43fd3a7
Aldijana Joffe <aldijanajoffe@gmail.com>
2020-11-12 15:46:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ function searchLocation(position) {
   let apiKey = "851984f8297b7be960398a279dde546f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+  
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
